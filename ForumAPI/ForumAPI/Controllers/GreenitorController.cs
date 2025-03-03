dbf1d1c26d842b69480d640415d2991eb08def6f
Tegarde <bruno.oliveira.1994@hotmail.com>
2025-03-03 14:53:05 +0000

    Fixed GreenitorController

--- ForumAPI/ForumAPI/Controllers/GreenitorController.cs
+++ ForumAPI/ForumAPI/Controllers/GreenitorController.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ForumAPI.Controllers
 {
-    public class GreenitorController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreenitorController : ControllerBase
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
     }
 }
