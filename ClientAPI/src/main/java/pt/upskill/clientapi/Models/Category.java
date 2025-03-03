996a7b8ac3b17949c16dccad3b481d8b8687329b
Tegarde <bruno.oliveira.1994@hotmail.com>
2025-03-03 10:24:07 +0000

    Added models to the Spring app

---
+++ ClientAPI/src/main/java/pt/upskill/clientapi/Models/Category.java
@@ -0,0 +1,34 @@
+package pt.upskill.clientapi.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Represents a category.
+ */
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Category {
+
+    /**
+     * Id of the category.
+     */
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private int id;
+
+    /**
+     * Description of the category.
+     */
+    @Column(length = 50, nullable = false)
+    private String description;
+}
