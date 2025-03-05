18146f9adfd8be70fcbb5658a5ca478d52dc5fe5
Alberto Rosa <albertorosaeng@gmail.com>
2025-03-05 11:52:19 +0000

    Create PostFavoriteDTO.cs

---
+++ ForumAPI/ForumAPI/DTOs/PostDTOs/PostFavoriteDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace ForumAPI.DTOs
+{
+    /// <summary>
+    /// Data Transfer Object for the PostFavorite entity.
+    /// </summary>
+    public class PostFavoriteDTO
+    {
+        /// <summary>
+        /// The post ID associated with the favorite.
+        /// </summary>
+        public int PostId { get; set; }
+
+        /// <summary>
+        /// The user associated with the favorite.
+        /// </summary>
+        public string User { get; set; }
+    }
+}
