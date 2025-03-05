7c43be0369db49d1e7a84a64b8a9ec01335b6150
Alberto Rosa <albertorosaeng@gmail.com>
2025-03-05 11:55:16 +0000

    Create PostDislikeDTO.cs

---
+++ ForumAPI/ForumAPI/DTOs/PostDTOs/PostDislikeDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace ForumAPI.DTOs
+{
+    /// <summary>
+    /// Data Transfer Object for the PostDislike entity.
+    /// </summary>
+    public class PostDislikeDTO
+    {
+        /// <summary>
+        /// The post ID associated with the dislike.
+        /// </summary>
+        public int PostId { get; set; }
+
+        /// <summary>
+        /// The user associated with the dislike.
+        /// </summary>
+        public string User { get; set; }
+    }
+}
