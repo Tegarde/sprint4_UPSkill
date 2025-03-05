162d617f221c78118e3dd9a0074bf522f8614c56
Tegarde <bruno.oliveira.1994@hotmail.com>
2025-03-05 14:47:48 +0000

    Fixed CreatePost
    Still needs to check the category and increment interactions of the user

--- ForumAPI/ForumAPI/Services/PostService.cs
+++ ForumAPI/ForumAPI/Services/PostService.cs
@@ -9,9 +9,9 @@ namespace ForumAPI.Services
     public class PostService : PostDAO
     {
         private readonly DataContext context;
-        private readonly GreenitorClient greenitorClient;
+        private readonly GreenitorDAO greenitorClient;
 
-        public PostService(DataContext context, HttpClient httpClient, GreenitorClient greenitorClient)
+        public PostService(DataContext context, GreenitorDAO greenitorClient)
         {
             this.context = context;
             this.greenitorClient = greenitorClient;
@@ -45,6 +45,8 @@ namespace ForumAPI.Services
                 throw new Exception("User does not exist. Cannot create post.");
             }
 
+            post.CreatedAt = DateTime.UtcNow;
+
             context.Posts.Add(post);
             await context.SaveChangesAsync();
 
@@ -73,6 +75,13 @@ namespace ForumAPI.Services
             await context.SaveChangesAsync();
         }
 
+        public async Task<List<Post>> SearchPostsByKeyword(string keyword)
+        {
+            var posts = await context.Posts
+                .Where(p => p.Title.Contains(keyword) || p.Content.Contains(keyword))
+                .ToListAsync();
 
+            return posts;
+        }
     }
 }
