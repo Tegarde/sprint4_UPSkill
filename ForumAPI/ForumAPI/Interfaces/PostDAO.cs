ac89b4df08b64c8464918c48b951fd52ebd24379
vasco-silva1 <vascosamuel.silva@hotmail.com>
2025-03-06 12:10:10 +0000

    DAOPOST update

--- ForumAPI/ForumAPI/Interfaces/PostDAO.cs
+++ ForumAPI/ForumAPI/Interfaces/PostDAO.cs
@@ -11,8 +11,19 @@ namespace ForumAPI.Interfaces
         Task<ActionResult> AddPostToFavorites(int id, string username);
 
         Task<ActionResult> RemovePostFromFavorites(int id, string username);
-        Task UpdatePostStatus(int id, bool newStatus);
 
         Task<List<Post>> SearchPostsByKeyword(string keyword);
+
+        Task<List<Post>> GetPostsByUser(string username);
+
+        Task<List<Post>> GetAllPosts();
+
+        Task<Post> UpdatePost(int postId, Post updatedPost);
+
+        List<Post> GetPostSortedByDate();
+
+        Task<List<Post>> GetTopPostsByInteractions(int topN);
+
+        Task<List<Post>> GetPostsBetweenDates(DateTime startDate, DateTime endDate);
     }
 }
