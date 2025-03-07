aa875ae607340ac1747286e8dd287c5382386644
Tegarde <bruno.oliveira.1994@hotmail.com>
2025-03-07 11:13:03 +0000

    Fixed some styling

--- Greenit/src/app/Components/Post/post-details/post-details.component.ts
+++ Greenit/src/app/Components/Post/post-details/post-details.component.ts
@@ -3,11 +3,12 @@ import { Post } from '../../../Models/post';
 import { DatePipe, NgFor, NgIf } from '@angular/common';
 import { CommentDetailsComponent } from '../../comment-details/comment-details.component';
 import { PostService } from '../../../Services/post.service';
+import { LikeDislikeComponent } from '../../like-dislike/like-dislike.component';
 
 @Component({
   selector: 'app-post-details',
   standalone: true,
-  imports: [NgIf, NgFor, DatePipe, CommentDetailsComponent],
+  imports: [NgIf, NgFor, DatePipe, CommentDetailsComponent, LikeDislikeComponent],
   templateUrl: './post-details.component.html',
   styleUrl: './post-details.component.css'
 })
@@ -17,7 +18,7 @@ export class PostDetailsComponent implements OnInit {
   constructor(private postService : PostService) { }
 
   ngOnInit(): void {
-    this.postService.getPostById(7).subscribe({
+    this.postService.getPostById(4).subscribe({
       next : (post) => {
         this.post = post;
       },
