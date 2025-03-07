20877b96df859469268cba3051f4d8f19ad42f78
DeviGan31 <902401130@portic.ipp.pt>
2025-03-07 11:48:36 +0000

    Angular Sign up created

--- Greenit/src/app/Components/sign-up/sign-up.component.ts
+++ Greenit/src/app/Components/sign-up/sign-up.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SignUpService } from '../../Services/sign-up.service';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
 
+  message?: string;
+
+  signUpForm : FormGroup;
+
+  constructor(private fb: FormBuilder, private signUpService: SignUpService, private router : Router) {
+    this.signUpForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(16)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(16)]],
+
+    });
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      this.signUpService.register(this.signUpForm.value).subscribe({
+        next: () => {
+          this.message = "User registered successfully";
+          this.signUpForm.reset();
+
+          setTimeout(() => {
+            this.message = "";
+            this.router.navigate(['']);
+          }, 1000);
+        },
+        error: () => {
+          this.message = "User registration failed";
+        }
+      });
+    }
+  }
 }
--- Greenit/src/app/app.routes.ts
+++ Greenit/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PostDetailsComponent } from './Components/Post/post-details/post-details.component';
 import { LikeDislikeComponent } from './Components/like-dislike/like-dislike.component';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
 
 export const routes: Routes = [
     { path : '', component : AppComponent },
     {path : 'posts', component : PostDetailsComponent},
-    {path : 'likes', component : LikeDislikeComponent}
+    {path : 'likes', component : LikeDislikeComponent},
+    {path : 'signup', component : SignUpComponent},
 ];
