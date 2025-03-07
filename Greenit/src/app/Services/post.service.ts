6599f4135c909a5be76b83aa3cc48d8b715a802e
Tegarde <bruno.oliveira.1994@hotmail.com>
2025-03-07 15:34:40 +0000

    Likes and dislikes working with API

--- Greenit/src/app/Services/post.service.ts
+++ Greenit/src/app/Services/post.service.ts
@@ -19,4 +19,32 @@ export class PostService {
   getPostById(id : number) : Observable<Post> {
     return this.client.get<Post>(`${this.endpoint}/${id}`);
   }
+
+  getPostInteractions(id : number) : Observable<any> {
+    return this.client.get<any>(`${this.endpoint}/likesAndDislikes/${id}`);
+  }
+
+  getInteractionByUser(id : number, username : string) : Observable<any> {
+    return this.client.get<any>(`${this.endpoint}/${id}/interactions/${username}`);
+  }
+
+  likeAPost(id : number, username : string) : Observable<any> {
+    const like = {postId : id, user : username};
+    return this.client.post<any>(`${this.endpoint}/like`, like);
+  }
+
+  dislikeAPost(id : number, username : string) : Observable<any> {
+    const dislike = {postId : id, user : username};
+    return this.client.post<any>(`${this.endpoint}/dislike`, dislike);
+  }
+
+  unlikeAPost(id : number, username : string) : Observable<any> {
+    const unlike = {postId : id, user : username};
+    return this.client.delete<any>(`${this.endpoint}/like`, {body : unlike});
+  }
+
+  undislikeAPost(id : number, username : string) : Observable<any> {
+    const undislike = {postId : id, user : username};
+    return this.client.delete<any>(`${this.endpoint}/dislike`, {body : undislike});
+  }
 }
