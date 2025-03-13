00143c8a394827d19c9ed2a4ff09cb926f9d21a8
DeviGan31 <902401130@portic.ipp.pt>
2025-03-13 03:16:31 +0000

    Update swaggerConfig.java

--- ClientAPI/src/main/java/pt/upskill/clientapi/SwaggerConfig.java
+++ ClientAPI/src/main/java/pt/upskill/clientapi/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package pt.upskill.clientapi;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +10,11 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "ClientAPI (SpringBoot)",
                 version = "1.0.0",
-                description = "API for managing client operations"
+                description = "API for managing forum-related operations",
+                contact = @Contact(
+                        name = "GitHub Repository",
+                        url = "https://github.com/Tegarde/sprint4_UPSkill"
+                )
         )
 )
 public class SwaggerConfig {
