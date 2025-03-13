a5c1506232e5eda8f8d3257747c60c1bb0652eee
Alberto Rosa <albertorosaeng@gmail.com>
2025-03-13 16:23:04 +0000

    Update CategoryClientTests.cs

--- ForumAPI/ForumAPITests/Services/CategoryClientTests.cs
+++ ForumAPI/ForumAPITests/Services/CategoryClientTests.cs
@@ -11,7 +11,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace ForumAPITests.Services
+namespace ForumAPI.Tests.Services
 {
     public class CategoryClientTests
     {
